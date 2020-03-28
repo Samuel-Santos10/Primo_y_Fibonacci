d228e8a301339b26d9f402db21ce73c7730cbf4d
Santos <Santos@DESKTOP-CNOGMDK>
2020-03-27 22:56:21 -0600

    actua

--- Primo_y_Fibonacci/Form1.Designer.cs
+++ Primo_y_Fibonacci/Form1.Designer.cs
@@ -35,11 +35,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Turquoise;
-            this.ClientSize = new System.Drawing.Size(464, 425);
+            this.ClientSize = new System.Drawing.Size(456, 444);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+           
             this.ResumeLayout(false);
 
         }
--- Primo_y_Fibonacci/Form1.cs
+++ Primo_y_Fibonacci/Form1.cs
@@ -14,69 +14,10 @@ namespace Primo_y_Fibonacci
     {
         public Form1()
         {
-            InitializeComponent();
-            fibonacci();
+        
         }
 
-        private void btncalcular_Click(object sender, EventArgs e)
-        {
-
-            int numero = byte.Parse(txtnum.Text);
-
-            int cont = 0;
-
-            for (int i = 1; i <= numero; i++)
-
-            {
-            
-           if (numero % i == 0)
-
-                {
-                    cont++;
-                }
-            }
-
-            if (cont == 2)
-            {
-                lblrespuesta.Text =  numero + (" Es PRIMO");
-
-            }
-
-            else
-
-            {
-                lblrespuesta.Text = numero + (" NO es primo");
-            }
-           
-        }
-
-        private void fibonacci ()
-
-        {
-
-            int pnumero = 0;
-            int snumero = 1;
-            int x;
-            int nlimite = 1000;
-
-            lstnumeros.Items.Add(pnumero);
-            lstnumeros.Items.Add(snumero);
-
-            while (snumero + pnumero <= nlimite)
-            {
-                x = pnumero;
-                pnumero = snumero;
-                snumero = x + pnumero;
-
-                lstnumeros.Items.Add(snumero);
-            }
+        
 
         }
-
-        private void btnnuevo_Click(object sender, EventArgs e)
-        {
-            lblrespuesta.Text = "";
-            txtnum.Text = "";
-        }
-    }
 }
